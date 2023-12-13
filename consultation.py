5fd9e6d919af8a2d641a68034c0d8d702d6d3581
Marco Colombo <mar.colombo13@gmail.com>
2023-12-13 14:38:18 +0100

    Parse command line arguments outside of the loop.

--- consultation.py
+++ consultation.py
@@ -6,6 +6,18 @@ from wand.image import Image as WImage
 from deep_translator import (GoogleTranslator)
 
 pytesseract.tesseract_cmd = r"tesseract"
+
+## command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help="path to the input image", default='{name}')
+ap.add_argument("-l", "--lang", help='path to the language', default='eng')
+ap.add_argument("-t", "--to", type=str, default="en", help="eng")
+ap.add_argument("-p", "--psm 3", type=int, default=13, help="Tesseract PSM mode")
+args = vars(ap.parse_args())
+
+## options for pytesseract
+options = "-l {} --psm 3 {}".format(args["lang"], args["psm 3"])
+
 list = ["im1.jpg", "img2.jpg"]
 for item in list:
     print("Processing image {}".format(item))
@@ -17,14 +29,6 @@ for item in list:
         offset=-0.15 * img.quantum_range)
         img.save(filename='gray_'+item)   
     # ### Extract the text 
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-i", "--image", help="path to the input image", default='{name}')
-        ap.add_argument("-l", "--lang", help='path to the language', default='eng')
-        ap.add_argument("-t", "--to", type=str, default="en", help="eng")
-        ap.add_argument("-p", "--psm 3", type=int, default=13, help="Tesseract PSM mode")
-        args = vars(ap.parse_args())
-    # ### Set-up columns
-        options = "-l {} --psm 3 {}".format(args["lang"], args["psm 3"])
         name='gray_'+item
         text = pytesseract.image_to_string(name)
     # ### Write-up original text
